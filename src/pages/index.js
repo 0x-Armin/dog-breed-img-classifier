df251f5512199c5c83aad778cd1824b99ed01bcd
pohjie <0xArlet@gmail.com>
2023-04-06 13:13:30 +0800

    Fix unescaped '

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
     <div id='main-container'>
       <h1>Dog Breed Classifier</h1>
       <p>Have you always wanted to find out what breed is that cute dog you saw on the street?</p>
-      <p>Here's Dog Breed Classifier- a tool that will help you to do just so!</p>
+      <p>Here&apos;s Dog Breed Classifier- a tool that will help you to do just so!</p>
       <DogBreedClassifier />
     </div>
   )
